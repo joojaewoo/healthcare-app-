8bbe5f2f9fa22140bb83329e8f863e136fcec461
Jae-Woo Joo <wnwodn4293@naver.com>
2019-11-05 18:58:59 +0900

    영상

--- app/src/main/java/com/cookandroid/healthcare/exercise.java
+++ app/src/main/java/com/cookandroid/healthcare/exercise.java
@@ -12,31 +12,40 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 public class exercise extends YouTubeBaseActivity {
-    private YouTubePlayerView youtubeViewer;
-    YouTubePlayer.OnInitializedListener listener;
+        public static final String API_KEY = "AIzaSyD4u3Vij9fkLSUpshIK80GJ5Ov8NLDWALc";//사용자가 얻은 API Key을 입력하면 된다.(개발자 콘솔에 얻은 것.)
+        private YouTubePlayerView youtubeViewer;
+        Button btn1, btn2, btn3, btn4;
+        public static String VIDEO_ID = "eJCn1mylqSk";
+        YouTubePlayer.OnInitializedListener listener;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                /** attaching layout xml **/
+                setContentView(R.layout.activity_exercise);
+                /** Initializing YouTube player view **/
+                youtubeViewer= (YouTubePlayerView) findViewById(R.id.youtubeViewer);
+                listener = new YouTubePlayer.OnInitializedListener() {
+                        @Override
+                        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                                youTubePlayer.loadVideo(VIDEO_ID);
+                        }
+                        @Override
+                        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                                Toast.makeText(exercise.this, "재생 실패", Toast.LENGTH_LONG).show();
+                        }
+                };
+                youtubeViewer.initialize(API_KEY,listener);
+                btn1 = findViewById(R.id.playBtn1);
+                btn2 = findViewById(R.id.playBtn2);
+                btn3 = findViewById(R.id.playBtn3);
+                btn4 = findViewById(R.id.playBtn4);
+                btn1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                VIDEO_ID="F9H1VjTa5is";
+                                youtubeViewer.initialize(API_KEY,listener);
+                        }
+                });
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button playBtn = findViewById(R.id.playBtn);
-        youtubeViewer = findViewById(R.id.youtubeViewer);
-
-        listener = new YouTubePlayer.OnInitializedListener() {
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("rWRC-9uAd_k");
-            }
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                Toast.makeText(exercise.this, "재생 실패", Toast.LENGTH_LONG).show();
-            }
-        };
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //youtubeViewer.initialize("AIzaSyD4u3Vij9fkLSUpshIK80GJ5Ov8NLDWALc",listener);
-            }
-        });
-    }
+        }
 }
